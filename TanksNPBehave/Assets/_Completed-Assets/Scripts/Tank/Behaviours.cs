e18f659d6d33a0741c66540664ec3fcad61449e3
Basher207 <basher207@gmail.com>
2017-10-26 09:55:29 +0300

    Added new behaviours
    Added the 4 required behaviours to the Behaviours.cs
    Tank and comments associated with them.
    
    Will be modifing comments a bit more to make them more intuitive.

--- TanksNPBehave/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
+++ TanksNPBehave/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
@@ -15,11 +15,14 @@ namespace Complete
         private Root CreateBehaviourTree() {
 
             switch (m_Behaviour) {
-
-                case 1:
-                    return SpinBehaviour(-0.05f, 1f);
-                case 2:
-                    return TrackBehaviour();
+				case 0:
+					return FunBehaviour();
+				case 1:
+					return DeadlyBehaviour();
+				case 2:
+					return FrightenedBehaviour();
+				case 3:
+					return UnpredictableBehaviour();
 
                 default:
                     return new Root (new Action(()=> Turn(0.1f)));
@@ -35,6 +38,7 @@ namespace Complete
         private Node RandomFire() {
             return new Action(() => Fire(UnityEngine.Random.Range(0.0f, 1.0f)));
         }
+
 
 
         /* Example behaviour trees */
@@ -70,6 +74,118 @@ namespace Complete
                 )
             );
         }
+
+
+
+
+
+
+
+
+		//Actions are stored as node functions so they can be reused in different behaviours 
+		public Node TargetUnit () {
+			//Updates the perception values every 0.1 seconds, run Selector 
+			return new Service (0.1f, UpdatePerception, 
+				//Splits the node, if target is to right, than Turn (0.3f) succeeds in running, and the tank turns right
+				//Otherwise tank turns left 
+				new Selector (
+					new BlackboardCondition ("targetOnRight",
+						Operator.IS_EQUAL, true,
+						Stops.NONE,
+						// Turn right toward target
+						new Action (() => Turn (0.3f))),
+					new Action (() => Turn (-0.3f))
+				)
+			);
+		}
+		public Node FireAndTurn (float fireVel = 0f) {
+			return new Sequence(
+				new Action(() => Turn(1f)),		//Sets rotation rate 
+				new Action(() => Fire(fireVel)) //Set shooting velocity at 0 for maximum fire rate
+			);
+		}
+		public Node DeadlyFireAndTurn () {
+			return new Sequence(
+				//(Using Mathf.Sin for movement and rotation allows for left and right, front and back movement in case tank gets stuck) 
+				new Action(() => Move(Mathf.Sin (Time.time * 2f))),	//Sets Movement rate
+				new Action(() => Turn(Mathf.Sin (Time.time))),	//Sets rotation rate 
+				new Action(() => Fire(0f)) 						//Sets shooting velocity at 0 to maximum fire rate
+			);
+		}
+		public Node RetreatAndFire (float fireVel = 0.5f) {
+			return new Sequence (
+					//(using Mathf.Sin (-0.6) means that the tank will mostly move backwards, but a bit forwards in case it's stuck)
+				new Action (() => Move (Mathf.Sin (Time.time) - 0.6f)),	
+				new Action (() => Fire (fireVel)), 						//Sets shooting velocity at 0 to maximum fire rate
+
+				TargetUnit ()
+			);
+		}
+		public Node SeekAndFire (float fireVel = 0.2f) {
+			return new Sequence (
+				//(using Mathf.Sin (-0.6) means that the tank will mostly move backwards, but a bit forwards in case it's stuck)
+				new Action (() => Move (1f)),	
+				new Action (() => Fire (fireVel)), 						//Sets shooting velocity at 0 to maximum fire rate
+
+				TargetUnit ()
+			);
+		}
+
+
+
+
+		private Root FunBehaviour() {
+			return new Root(FireAndTurn ());
+		}
+		private Root DeadlyBehaviour() {
+			return new Root(DeadlyFireAndTurn ());
+		}
+		private Root FrightenedBehaviour() {
+			return new Root(RetreatAndFire ());
+		}
+		private Root UnpredictableBehaviour() {
+			//Modulars time to loop actions throughtout 20 second intervals
+			//If modular time is < 4, FireAndTurn
+			//else If modular time is > 16, DeadlyFireAndTurn
+			//else If modular time is < 10, RetreatAndFire
+			//else If modular time is < 5, SeekAndFire
+			return new Root(
+				//Selector used so only 1 node is excuted at a time
+				new Selector (
+					new Condition (() => {return (Time.time % 20 < 4f);}, Stops.IMMEDIATE_RESTART, FireAndTurn ()),
+					new Condition (() => {return (Time.time % 20 > 16);}, Stops.IMMEDIATE_RESTART, DeadlyFireAndTurn ()),
+					new Condition (() => {return (Time.time % 20 > 10f);}, Stops.IMMEDIATE_RESTART, RetreatAndFire ()),
+					new Condition (() => {return (Time.time % 20 > 5f);}, Stops.IMMEDIATE_RESTART, SeekAndFire ()),
+					TargetUnit ()
+				)
+			);
+
+		}
+
+
+
+//		private Root TurnTowardsNearestTank(float turn, float shoot) {
+//			int playerLayer = LayerMask.GetMask ("Players");
+//			Collider [] colls = Physics.OverlapSphere (transform.position, 1000f, playerLayer);
+//
+//			Collider targetColl = null;
+//
+//			for (int i = 0; i < colls.Length; i++) {
+//				if (colls [i].gameObject != gameObject) {
+//					targetColl = colls [i];
+//				}
+//			}
+//			if (targetColl) {
+//				Quaternion rotationAxis = Quaternion.AngleAxis (1f, transform.up);
+//				return new Root (new Sequence (
+//					new Action (() => Turn (turn)),
+//					new Action (() => Fire (shoot))
+//				));
+//
+//			} else {
+//				return SpinBehaviour(-0.05f, 1f);
+//			}
+//		}
 
         private void UpdatePerception() {
             Vector3 targetPos = TargetTransform().position;
